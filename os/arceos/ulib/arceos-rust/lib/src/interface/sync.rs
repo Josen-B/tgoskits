Shl1015CS/ArceOS

refactor[rust-std]: refactor task and thread related interfaces

ulib/arceos-rust/lib/src/interface/sync.rs
@@ -1,0 +1,22 @@
+use arceos_api::modules::axlog::info;
+use arceos_posix_api::ctypes::timespec;
+
+#[unsafe(no_mangle)]
+pub fn sys_futex_wait(
+    address: *mut u32,
+    expected: u32,
+    timeout: *const timespec,
+    flags: u32,
+) -> i32 {
+    // sys_futex_wait(address, expected, timeout, flags);
+    // Placeholder implementation
+    // info!("called sys_futex_wait");
+    0
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_futex_wake(address: *mut u32, count: i32) -> i32 {
+    // Placeholder implementation
+    info!("called sys_futex_wake");
+    0
+}

ulib/arceos-rust/lib/src/interface/task.rs
@@ -1,15 +1,21 @@
+#[cfg(feature = "multitask")]
+use crate::interface::thread::take_task;
 use arceos_api::modules::axlog::{ax_println, info};
 use arceos_api::sys::ax_terminate;
 
 #[unsafe(no_mangle)]
 pub fn sys_abort() -> ! {
     info!("called sys_abort");
+    #[cfg(feature = "multitask")]
+    take_task(arceos_api::task::ax_current_task_id());
     ax_terminate()
 }
 
 #[unsafe(no_mangle)]
 pub fn sys_exit(code: i32) -> ! {
     info!("called sys_exit with code {}", code);
+    #[cfg(feature = "multitask")]
+    take_task(arceos_api::task::ax_current_task_id());
     ax_println!("[ArceOS] Process exited with code {}", code);
     ax_terminate()
 }

ulib/arceos-rust/lib/src/interface/util.rs
@@ -1,0 +1,41 @@
+use arceos_api::modules::axhal::time::wall_time_nanos;
+use arceos_api::modules::axlog::info;
+use arceos_posix_api::ctypes::{clockid_t, timespec};
+use core::time::Duration;
+use rand::prelude::SmallRng;
+use rand::{RngCore, SeedableRng};
+
+/// Fill `len` bytes in `buf` with cryptographically secure random data.
+///
+/// Returns either the number of bytes written to buf (a positive value) or
+/// * `-EINVAL` if `flags` contains unknown flags.
+/// * `-ENOSYS` if the system does not support random data generation.
+#[unsafe(no_mangle)]
+pub fn sys_read_entropy(buf: *mut u8, len: usize, _flags: u32) -> isize {
+    // flags are currently ignored
+    info!("called sys_read_entropy");
+    let buffer = unsafe { core::slice::from_raw_parts_mut(buf, len) };
+    let mut rng = SmallRng::seed_from_u64(wall_time_nanos());
+    rng.fill_bytes(buffer);
+    len as isize
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_clock_gettime(clockid: clockid_t, tp: *mut timespec) -> i32 {
+    info!("called sys_clock_gettime with clockid {}, tp {:p}", clockid, tp);
+    unsafe { arceos_posix_api::sys_clock_gettime(clockid, tp) }
+}
+
+/// suspend execution for microsecond intervals
+///
+/// The usleep() function suspends execution of the calling
+/// thread for (at least) `usec` microseconds.
+#[unsafe(no_mangle)]
+pub fn sys_usleep(usec: u64) {
+    info!("called sys_usleep with {} usec", usec);
+    let duration = Duration::from_micros(usec);
+    #[cfg(feature = "multitask")]
+    arceos_api::modules::axtask::sleep(duration);
+    #[cfg(not(feature = "multitask"))]
+    arceos_api::modules::axhal::time::busy_wait(duration);
+}

Josen-B/tgoskits

os/arceos/ulib/arceos-rust/lib/src/interface/sync.rs
@@ -1,0 +1,22 @@
+use ax_api::modules::ax_log::info;
+use ax_posix_api::ctypes::timespec;
+
+#[unsafe(no_mangle)]
+pub fn sys_futex_wait(
+    address: *mut u32,
+    expected: u32,
+    timeout: *const timespec,
+    flags: u32,
+) -> i32 {
+    // sys_futex_wait(address, expected, timeout, flags);
+    // Placeholder implementation
+    // info!("called sys_futex_wait");
+    0
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_futex_wake(address: *mut u32, count: i32) -> i32 {
+    // Placeholder implementation
+    info!("called sys_futex_wake");
+    0
+}

os/arceos/ulib/arceos-rust/lib/src/interface/task.rs
@@ -1,15 +1,21 @@
+#[cfg(feature = "multitask")]
+use crate::interface::thread::take_task;
 use ax_api::modules::ax_log::{ax_println, info};
 use ax_api::sys::ax_terminate;
 
 #[unsafe(no_mangle)]
 pub fn sys_abort() -> ! {
     info!("called sys_abort");
+    #[cfg(feature = "multitask")]
+    take_task(ax_api::task::ax_current_task_id());
     ax_terminate()
 }
 
 #[unsafe(no_mangle)]
 pub fn sys_exit(code: i32) -> ! {
     info!("called sys_exit with code {}", code);
+    #[cfg(feature = "multitask")]
+    take_task(ax_api::task::ax_current_task_id());
     ax_println!("[ArceOS] Process exited with code {}", code);
     ax_terminate()
 }

os/arceos/ulib/arceos-rust/lib/src/interface/util.rs
@@ -1,0 +1,41 @@
+use ax_api::modules::ax_hal::time::wall_time_nanos;
+use ax_api::modules::ax_log::info;
+use ax_posix_api::ctypes::{clockid_t, timespec};
+use core::time::Duration;
+use rand::prelude::SmallRng;
+use rand::{RngCore, SeedableRng};
+
+/// Fill `len` bytes in `buf` with cryptographically secure random data.
+///
+/// Returns either the number of bytes written to buf (a positive value) or
+/// * `-EINVAL` if `flags` contains unknown flags.
+/// * `-ENOSYS` if the system does not support random data generation.
+#[unsafe(no_mangle)]
+pub fn sys_read_entropy(buf: *mut u8, len: usize, _flags: u32) -> isize {
+    // flags are currently ignored
+    info!("called sys_read_entropy");
+    let buffer = unsafe { core::slice::from_raw_parts_mut(buf, len) };
+    let mut rng = SmallRng::seed_from_u64(wall_time_nanos());
+    rng.fill_bytes(buffer);
+    len as isize
+}
+
+#[unsafe(no_mangle)]
+pub fn sys_clock_gettime(clockid: clockid_t, tp: *mut timespec) -> i32 {
+    info!("called sys_clock_gettime with clockid {}, tp {:p}", clockid, tp);
+    unsafe { ax_posix_api::sys_clock_gettime(clockid, tp) }
+}
+
+/// suspend execution for microsecond intervals
+///
+/// The usleep() function suspends execution of the calling
+/// thread for (at least) `usec` microseconds.
+#[unsafe(no_mangle)]
+pub fn sys_usleep(usec: u64) {
+    info!("called sys_usleep with {} usec", usec);
+    let duration = Duration::from_micros(usec);
+    #[cfg(feature = "multitask")]
+    arceos_api::modules::axtask::sleep(duration);
+    #[cfg(not(feature = "multitask"))]
+    ax_api::modules::ax_hal::time::busy_wait(duration);
+}